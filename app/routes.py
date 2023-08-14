5b30658ddcb1606b21cc11fb672ab8aaef95656b
christian sanabria <ckevin89@hotmail.com>
2023-08-14 14:45:38 -0500

    add bootstrap and nav bar

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from app import app
+from flask import render_template
 
 # Add a route
 @app.route('/')
 def index():
-    return 'Hello World!!!!!!!!'
+    countries = ['United States', 'Canada', 'Mexico', 'France', 'Egypt', 'China']
+    return render_template('index.html', first_name='David', countries=countries)
 
 @app.route('/new')
 def new():
